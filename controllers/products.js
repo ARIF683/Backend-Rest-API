8ba04ecc2cbe535d9b9d63161403ffa79af6ea27
ARIF ALAM <70383209+ARIF683@users.noreply.github.com>
2023-03-19 17:07:53 +0000

    req.params added.

--- controllers/products.js
+++ controllers/products.js
@@ -2,12 +2,12 @@ const ProductModels = require("../models/products");
 
 
 const getAllProduct = async (req,res) => {
-    const Products = await ProductModels.find({})
+    const Products = await ProductModels.find(req.query)
     res.status(200).json({ Products })
 }
 
 const getAllProductTesting = async (req,res) => {
-    const ProductsTesting = await ProductModels.find({company: "samsung"})
+    const ProductsTesting = await ProductModels.find(req.query)
     res.status(200).json({ ProductsTesting})
 }
 
